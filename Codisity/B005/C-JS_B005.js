a81a94a753cc006ab36001fed964c4c26e7eb5b8
Marcin Adamiak <marcin_89@outlook.com>
2023-02-16 20:50:20 +0100

    Problem no 5

---
+++ Codisity/B005/C-JS_B005.js
@@ -0,0 +1,11 @@
+function addToArray(item, array) {
+	const newArray = array;
+	newArray.push(item);
+
+	return newArray;
+}
+
+const fruits = ["apple", "orange"];
+const favFruits = addToArray("banana", fruits);
+
+console.log({ fruits, favFruits });
