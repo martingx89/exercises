5f27d015946e4b81c0ded55b835fc02bb5060c05
Marcin Adamiak <marcin_89@outlook.com>
2023-02-17 17:49:04 +0100

    Resolve

--- Codisity/B021/C-JS_B021.js
+++ Codisity/B021/C-JS_B021.js
@@ -2,15 +2,21 @@ import fetch from "node-fetch";
 
 fetch("https://api.spacexdata.com/v4/404")
 	.then(response => {
+		if (!response.ok) {
+			throw new Error("Response not ok", {
+				cause: response,
+			});
+		}
+
 		return response.json();
 	})
 	.then(json => {
 		console.log(json);
 	})
 	.catch(error => {
-		if (error.status === 404) {
+		if (error.cause.status === 404) {
 			return console.error("Page not found");
 		}
 
-		return console.error(error);
+		console.error(error);
 	});
