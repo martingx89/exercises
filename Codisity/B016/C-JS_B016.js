955a5c716d73add7ec881ac043f1f9498c26565b
Marcin Adamiak <59664314+martingx89@users.noreply.github.com>
2023-02-17 09:45:13 +0000

    Problem no 16

---
+++ Codisity/B016/C-JS_B016.js
@@ -0,0 +1,15 @@
+const myDog = new Dog();
+const myCat = new Cat();
+
+function Dog() {
+	this.name = "Pluto";
+}
+
+class Cat {
+	constructor() {
+		this.name = "Garfield";
+	}
+}
+
+console.log(myDog.name);
+console.log(myCat.name);
