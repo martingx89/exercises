cfd28678034bc5c3f84291d99c31202afd624608
Marcin Adamiak <59664314+martingx89@users.noreply.github.com>
2023-02-17 08:11:09 +0000

    Problem no 11

---
+++ Codisity/B011/C-JS_B011.js
@@ -0,0 +1,20 @@
+const loggedUser = {
+	role: "reader",
+};
+
+function getUserPermission(user) {
+	let permission;
+
+	switch (user.role) {
+		case "reader":
+			permission = "can read";
+		case "writer":
+			permission = "can write";
+		case "admin":
+			permission = "can everything";
+	}
+
+	return permission;
+}
+
+console.log(getUserPermission(loggedUser));
