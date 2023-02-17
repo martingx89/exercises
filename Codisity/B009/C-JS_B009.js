b85962c85e8ad9cbf496a93d3dae12091bfffe53
Marcin Adamiak <59664314+martingx89@users.noreply.github.com>
2023-02-17 07:45:34 +0000

    Problem no 9

---
+++ Codisity/B009/C-JS_B009.js
@@ -0,0 +1,10 @@
+function reducer(accumulator, currentValue) {
+	currentValue += 1;
+
+	return accumulator + currentValue;
+}
+
+const numbers = [1, 2, 3];
+const sum = numbers.reduce(reducer);
+
+console.log(sum);
