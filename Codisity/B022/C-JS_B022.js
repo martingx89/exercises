47150eb438acb2d40ca115c0316dd8403a636110
Marcin Adamiak <marcin_89@outlook.com>
2023-02-18 22:17:37 +0100

    Resolve

--- Codisity/B022/C-JS_B022.js
+++ Codisity/B022/C-JS_B022.js
@@ -1,6 +1,6 @@
-const html = "
+const html = `
 <h1>Hello World!</h1>
 <p>JavaScript is awesome!</p>
-"
+`;
 
-document.body.insertAdjacentHTML("afterbegin", html)
+document.body.insertAdjacentHTML("afterbegin", html);
