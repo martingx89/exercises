87921b5e3ddf4995629f673f58ed0f52582615cd
Marcin Adamiak <marcin_89@outlook.com>
2023-08-15 19:30:47 +0200

    Try resolve

--- Codisity/B026/C-JS_B026.js
+++ Codisity/B026/C-JS_B026.js
@@ -1,20 +1,22 @@
-// let firstName = new String("Brendan")
-// let lastName = String("Eich")
-// let user = { id: 1, admin: true }
-// let numbers = [1, 2, 3]
-// let nothing
-// let empty = null
-// let greet = function () { return "Hi!" }
-// let greeting = new greet()
-//
-// console.log("object" === typeof firstName)
-// console.log("object" === typeof lastName)
-// console.log("object" === typeof user)
-// console.log("object" === typeof numbers)
-// console.log("object" === typeof empty)
-// console.log("object" === typeof nothing)
-// console.log("object" === typeof greet)
-// console.log("object" === typeof greeting)
+let firstName = new String('Brendan');
+let lastName = String('Eich');
+let user = { id: 1, admin: true };
+let numbers = [1, 2, 3];
+let nothing;
+let empty = null;
+let greet = function () {
+  return 'Hi!';
+};
+let greeting = new greet();
+
+console.log('object' === typeof firstName); //true
+console.log('object' === typeof lastName); //false
+console.log('object' === typeof user); //true
+console.log('object' === typeof numbers); //false
+console.log('object' === typeof empty); //true
+console.log('object' === typeof nothing); //false
+console.log('object' === typeof greet); //true
+console.log('object' === typeof greeting); //true
 
 // Które z powyższych porównań zwrócą prawdę, a które fałsz?
 // Dla jakiej wartości działanie typeof zwróci błędną informację?
