00c869c1bc42844f3e1fecf1216086eee66cc9c8
Marcin Adamiak <marcin_89@outlook.com>
2023-10-08 10:39:04 +0200

    Fix: add constants

--- Skrypty/Destructuring.js
+++ Skrypty/Destructuring.js
@@ -1,7 +1,10 @@
 const FILIP = { name: 'Filip', age: '29', city: 'Kraków' };
 
 const getPersonalInfo = (person) => {
-  return `Imię: ${person.name}, wiek: ${person.age}, miasto: ${person.city}`;
+  const name = person.name;
+  const age = person.age;
+  const city = person.city;
+  return `Imię: ${name}, wiek: ${age}, miasto: ${city}`;
 };
 
 console.log(getPersonalInfo(FILIP));
