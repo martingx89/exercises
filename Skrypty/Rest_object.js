5ccb1bd95274e76355bb2c2ae43ac8c8e9d16134
Marcin Adamiak <marcin_89@outlook.com>
2023-10-08 11:24:22 +0200

    Add: comment

--- Skrypty/Rest_object.js
+++ Skrypty/Rest_object.js
@@ -1,5 +1,5 @@
 const person = { name: 'Filip', age: 29, city: 'Kraków', id: 2549 };
 
-const { id, ...personInfo } = person;
+const { id, ...personInfo } = person; //rest operator musi być na końcu
 console.log(id);
 console.log(personInfo);
