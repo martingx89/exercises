7cbb5e9acf23ca7aed0bd44fe083a0bac62ed67e
Marcin Adamiak <marcin_89@outlook.com>
2023-10-08 10:42:13 +0200

    Refactor: delete comment code

--- Skrypty/Destructuring.js
+++ Skrypty/Destructuring.js
@@ -1,10 +1,6 @@
 const FILIP = { name: 'Filip', age: '29', city: 'Kraków' };
 
 const getPersonalInfo = (person) => {
-  //   const name = person.name;
-  //   const age = person.age;
-  //   const city = person.city;
-
   const [name, age, city] = person;
 
   return `Imię: ${name}, wiek: ${age}, miasto: ${city}`;
