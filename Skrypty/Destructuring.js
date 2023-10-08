e22648cc014489ab84f3d1b09458a6cba6a23421
Marcin Adamiak <marcin_89@outlook.com>
2023-10-08 10:36:54 +0200

    Fix: show all data

--- Skrypty/Destructuring.js
+++ Skrypty/Destructuring.js
@@ -1,7 +1,7 @@
 const FILIP = { name: 'Filip', age: '29', city: 'Kraków' };
 
 const getPersonalInfo = (person) => {
-  return `Imię: ${person.name}`;
+  return `Imię: ${person.name}, wiek: ${person.age}, miasto: ${person.city}`;
 };
 
 console.log(getPersonalInfo(FILIP));
