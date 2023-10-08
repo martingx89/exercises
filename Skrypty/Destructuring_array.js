e74c13b20623ed0dc02fee4dac77d825cfece6a7
Marcin Adamiak <marcin_89@outlook.com>
2023-10-08 11:04:20 +0200

    Refactor: use destructuring as parameter

--- Skrypty/Destructuring_array.js
+++ Skrypty/Destructuring_array.js
@@ -1,13 +1,8 @@
 const FILIP = ['Filip', 29, 'Kraków'];
 
-const getPersonInfo = (person) => {
-  // const name = person[0];
-  // const age = person[1];
-  // const city = person[2];
-
-  const [name, , city] = person; //kolejność ma znaczenie!
-
-  return `Imię: ${name}, miasto: ${city}`;
+const getPersonInfo = ([name, age, city]) => {
+  //kolejność ma znaczenie!
+  return `Imię: ${name}, wiek: ${age} miasto: ${city}`;
 };
 
 console.log(getPersonInfo(FILIP));
