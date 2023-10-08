eaa022f5f5d1fa0e9a475c1b325b12346f3795dd
Marcin Adamiak <marcin_89@outlook.com>
2023-10-08 11:26:46 +0200

    Add: inside object

--- Skrypty/Rest_object.js
+++ Skrypty/Rest_object.js
@@ -1,4 +1,12 @@
-const person = { name: 'Filip', age: 29, city: 'Kraków', id: 2549 };
+const person = {
+  name: 'Filip',
+  age: 29,
+  city: {
+    name: 'Kraków',
+    population: 800_000,
+  },
+  id: 2549,
+};
 
 const { id, ...personInfo } = person; //rest operator musi być na końcu
 console.log(id);
