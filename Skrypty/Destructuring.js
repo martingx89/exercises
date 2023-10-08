620b88342ce28768f0e54b6082bda8e6672755db
Marcin Adamiak <marcin_89@outlook.com>
2023-10-08 10:40:32 +0200

    Add: use destructuring

--- Skrypty/Destructuring.js
+++ Skrypty/Destructuring.js
@@ -1,9 +1,12 @@
 const FILIP = { name: 'Filip', age: '29', city: 'Kraków' };
 
 const getPersonalInfo = (person) => {
-  const name = person.name;
-  const age = person.age;
-  const city = person.city;
+  //   const name = person.name;
+  //   const age = person.age;
+  //   const city = person.city;
+
+  const [name, age, city] = person;
+
   return `Imię: ${name}, wiek: ${age}, miasto: ${city}`;
 };
 
