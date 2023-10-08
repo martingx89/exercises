83d9d3c939d2a0864f02ccc30535f77d9ce58492
Marcin Adamiak <marcin_89@outlook.com>
2023-10-08 10:54:05 +0200

    Add: function to show info

--- Skrypty/Destructuring_array.js
+++ Skrypty/Destructuring_array.js
@@ -1,1 +1,7 @@
 const FILIP = ['Filip', 29, 'Kraków'];
+
+const getPersonInfo = (person) => {
+  return `Imię: ${person[0]}, wiek: ${person[1]}, miasto: ${person[2]}`;
+};
+
+console.log(getPersonInfo(FILIP));
