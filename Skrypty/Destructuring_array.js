2a6187968c0a1936d5a2c04329cf7acb690153a3
Marcin Adamiak <marcin_89@outlook.com>
2023-10-08 11:05:22 +0200

    Add: use default value

--- Skrypty/Destructuring_array.js
+++ Skrypty/Destructuring_array.js
@@ -1,6 +1,6 @@
-const FILIP = ['Filip', 29, 'Kraków'];
+const FILIP = ['Filip', 29];
 
-const getPersonInfo = ([name, age, city]) => {
+const getPersonInfo = ([name, age, city = 'Warszawa']) => {
   //kolejność ma znaczenie!
   return `Imię: ${name}, wiek: ${age} miasto: ${city}`;
 };
